12ac393aa40982beb6f3703b32c753e249ab1200
NANDIGAMA PRASHANTH KUMAR <96291949+nandigamaprashanthkumar@users.noreply.github.com>
2023-12-13 19:58:42 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// calculator.js
+let displayValue = '';
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById('display').value = displayValue;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
